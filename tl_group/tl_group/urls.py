01342341ea75dcf1865ce275689e5b770c98c36f
andrey.chupakhin <andrey.chupakhin@edgecenter.ru>
2023-09-27 18:40:30 +0500

    add task

--- tl_group/tl_group/urls.py
+++ tl_group/tl_group/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tree/', include('employees.urls'))
+    path('employees/', include('employees.urls'))
 ]
-
